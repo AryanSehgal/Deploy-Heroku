1b336d03cef9d914e34a0c1ff3ec20214669370f
AryanSehgal <59551957+AryanSehgal@users.noreply.github.com>
2022-03-16 15:45:58 +0530

    Update Caption_it.py

--- Caption_it.py
+++ Caption_it.py
@@ -1,14 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
 
 
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import keras
-import json
 import pickle
 from keras.applications.vgg16 import VGG16
 from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
@@ -19,27 +11,15 @@ from keras.utils import to_categorical
 from keras.layers import Input, Dense, Dropout, Embedding, LSTM
 from keras.layers.merge import add
 
-
-# In[4]:
-
-
 model = load_model("model_weights/model_9.h5")
 model._make_predict_function()
 
-# In[6]:
-
-
 model_temp = ResNet50(weights="imagenet", input_shape=(224,224,3))
 
-
-# In[7]:
-
-
-# Create a new model, by removing the last layer (output layer of 1000 classes) from the resnet50
 model_resnet = Model(model_temp.input, model_temp.layers[-2].output)
 model_resnet._make_predict_function()
 
-# In[8]:
+
 
 
 def preprocess_image(img):
@@ -50,8 +30,6 @@ def preprocess_image(img):
     return img
 
 
-# In[15]:
-
 
 def encode_image(img):
     img = preprocess_image(img)
@@ -60,13 +38,6 @@ def encode_image(img):
     return feature_vector
 
 
-# In[ ]:
-
-
-
-
-
-# In[25]:
 
 
 with open("./storage/word_to_idx.pkl", 'rb') as w2i:
@@ -76,19 +47,6 @@ with open("./storage/idx_to_word.pkl", 'rb') as i2w:
     idx_to_word = pickle.load(i2w)
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[28]:
 
 
 def predict_caption(photo):
@@ -114,7 +72,6 @@ def predict_caption(photo):
     return final_caption
 
 
-# In[29]:
 
 
 def caption_this_image(image):
@@ -123,10 +80,5 @@ def caption_this_image(image):
     caption = predict_caption(enc)
     
     return caption
-
-
-# In[ ]:
-
-
 
 
